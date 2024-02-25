f6c59ad177eb214784e7c6232608d1541ac326f5
Breno <breno.am2004@gmail.com>
2024-02-24 23:43:17 -0300

    refac

--- static/js/theme-changer.js
+++ static/js/theme-changer.js
@@ -1,11 +1,13 @@
-document.getElementById("theme-int").addEventListener("click", function (event) {
+const theme_int = document.getElementById("theme-int")
+
+theme_int.addEventListener("click", function (event) {
     event.preventDefault();
 
-    document.getElementById("theme-int").classList.toggle("light");
+    theme_int.classList.toggle("light");
     document.body.classList.toggle("light-theme");
 
-    var lightImages = document.querySelectorAll(".light-img");
-    var darkImages = document.querySelectorAll(".dark-img");
+    let lightImages = document.querySelectorAll(".light-img");
+    let darkImages = document.querySelectorAll(".dark-img");
 
     lightImages.forEach(function (image) {
         image.style.display = image.style.display === "none" ? "block" : "none";
